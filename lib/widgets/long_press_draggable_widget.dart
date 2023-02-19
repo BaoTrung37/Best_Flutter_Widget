edcc6517f9b63a7480775fdc9145ca5d8c3de35e
BAOTRUNG\kuted <baotrung372k@gmail.com>
2023-02-19 16:06:12 +0700

    add long_press_deaggable_widget

--- lib/widgets/long_press_draggable_widget.dart
+++ lib/widgets/long_press_draggable_widget.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class LongPressDraggableWidget extends StatefulWidget {
   const LongPressDraggableWidget({super.key});
@@ -10,8 +9,43 @@ class LongPressDraggableWidget extends StatefulWidget {
 }
 
 class _LongPressDraggableWidgetState extends State<LongPressDraggableWidget> {
+  Offset _offset = const Offset(200, 200);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Stack(
+          children: [
+            Positioned(
+              left: _offset.dx,
+              top: _offset.dy,
+              child: LongPressDraggable(
+                feedback: Image.network(
+                  'https://tinyurl.com/95ncjeuu',
+                  height: 200,
+                  color: Colors.orangeAccent,
+                  colorBlendMode: BlendMode.colorBurn,
+                ),
+                onDragEnd: (details) {
+                  setState(() {
+                    double adjustment = MediaQuery.of(context).size.height -
+                        constraints.maxHeight;
+                    debugPrint(adjustment.toString());
+                    _offset = Offset(
+                      details.offset.dx,
+                      details.offset.dy - adjustment,
+                    );
+                  });
+                },
+                child: Image.network(
+                  'https://tinyurl.com/95ncjeuu',
+                  height: 200,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
